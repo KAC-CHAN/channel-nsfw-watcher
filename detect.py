3a53998537dbdd7bc1ac1bbf310e6115e97aa325
KAC CHAN <kacchan@outlook.in>
2023-12-13 19:35:13 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,7 +1,9 @@
 #Telegram @izuya
 
 import pyrogram
-from nsfw_detector import detect
+from deeppavlov import configs, build_model
+
+classifier = build_model(configs.nsfw.nsfw_bert)
 
 api_id = 26788480
 api_hash = "858d65155253af8632221240c535c314"
@@ -15,21 +17,13 @@ bot = pyrogram.Client("nsfw_channel_bot",
 @bot.on_message()
 async def check_nsfw(client, message):
 
-    # Check only messages in channel
-    if not message.forward_from_chat:
-        return
+  media = message.photo # etc
 
-    # Get media from message if any
-    media = message.photo or message.animation or message.video or message.sticker 
-
-    # Detect NSFW
-    if media:
-        is_nsfw = detect(media)
-    else:
-        is_nsfw = False
-
-    # Delete message if detected as NSFW
-    if is_nsfw:
-        await message.delete()
+  if media:
+    prediction = classifier({'image': media})['prediction']
+    is_nsfw = prediction == 'nsfw'
+  
+  if is_nsfw:
+    await message.delete()
 
 bot.run()
