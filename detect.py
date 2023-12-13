291cd5a047bd3e3c872d779a0a73564eb7277b8c
KAC CHAN <kacchan@outlook.in>
2023-12-13 19:27:51 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -3,10 +3,14 @@
 import pyrogram
 from nsfw_detector import detect
 
-api_id = 12345
-api_hash = "abc123"
+api_id = 26788480
+api_hash = "858d65155253af8632221240c535c314"
+bot_token = "6551178239:AAGWE_oTVdeyoYGxCCabwdiN1xOFpfj3FfE"
 
-bot = pyrogram.Client("nsfw_channel_bot", api_id, api_hash)
+bot = pyrogram.Client("nsfw_channel_bot", 
+                      api_id,  
+                      api_hash,
+                      bot_token=bot_token)
 
 @bot.on_message()
 async def check_nsfw(client, message):
